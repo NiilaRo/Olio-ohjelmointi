ea02a8c4a071e768468facbc902b91de00e14f61
NiilaRo <142781127+NiilaRo@users.noreply.github.com>
2024-08-19 23:59:42 +0300

    Create simpleC++Program.cpp

---
+++ simpleC++Program.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+
+int main() {
+    std::vector<int> numbers;
+
+    int count;
+    std::cout << "How many numbers do you want to enter? ";
+    std::cin >> count;
+
+    for (int i = 0; i < count; i++) {
+        int number;
+        std::cout << "Enter nr[" << (i + 1) << "]: ";
+        std::cin >> number;
+        numbers.push_back(number);
+    }
+
+    std::cout << "You entered: ";
+    for (int num : numbers) {
+        std::cout << num << " ";
+    }
+    std::cout << std::endl;
+
+    return 0;
+}
